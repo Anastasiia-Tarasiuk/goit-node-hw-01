8e290a8d048e0985a1ab27fdaa45c802e51c6008
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2022-09-07 13:21:34 +0300

    Adds readContacts function

--- contacts.js
+++ contacts.js
@@ -5,12 +5,19 @@ const id = uuidv4();
 
 const contactsPath = path.resolve('db/contacts.json');
 
-async function listContacts() {
+async function readContacts() {
     try {
         const data = await fs.readFile(contactsPath, 'utf8');
-        const contacts = JSON.parse(data);
-        console.table(contacts);
-        
+        return JSON.parse(data);     
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function listContacts() {
+    try {
+        const contacts = await readContacts();
+        console.table(contacts);        
     } catch (error) {
         console.log(error);
     }
@@ -18,8 +25,7 @@ async function listContacts() {
 
 async function getContactById(contactId) {
     try {
-        const data = await fs.readFile(contactsPath, 'utf8');
-        const contacts = JSON.parse(data);
+        const contacts = await readContacts();
         contacts.find(contact => {
             if (contact.id === contactId) {
                 console.table(contact);
@@ -32,8 +38,7 @@ async function getContactById(contactId) {
 
 async function removeContact(contactId) {
     try {
-        const data = await fs.readFile(contactsPath, 'utf8');
-        const contacts = JSON.parse(data);
+        const contacts = await readContacts();
         const filteredContacts = contacts.filter(contact => contact.id !== contactId)
         console.table(filteredContacts);
     } catch (error) {
@@ -43,14 +48,13 @@ async function removeContact(contactId) {
 
 async function addContact(name, email, phone) {
     try {
-        const data = await fs.readFile(contactsPath, 'utf8');
-        const prevContacts = JSON.parse(data);
+        const prevContacts = await readContacts();
         const newContact = { id, name, email, phone };
         const allContacts = [...prevContacts, newContact];
 
         await fs.writeFile(contactsPath, JSON.stringify(allContacts), 'utf8');
         console.table(`Contact ${newContact.name} was added succesfully`);
-        console.table(newContact);
+        console.table(allContacts);
     } catch (error) {
         console.log(error); 
     }
